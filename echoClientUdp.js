e5f34c61b1f8a74460317302855c9d4c763ef5d4
i310934 <j.pan@sap.com>
2016-10-30 19:18:56 +0800

    add random

--- echoClientUdp.js
+++ echoClientUdp.js
@@ -1,27 +1,26 @@
 var dgram = require('dgram');
-
 var clientSocket = dgram.createSocket('udp4');
-
-var messages = [
-    /*   'temperture:27',
-      'Are you OK?',
-      'I am happy.',
-      'A little panda found a pumpkin.' */
-    'A01wd024.0sd069.6%B',
-    'A42X+01710Y-02898Z-16846B'
-];
-
 var index = 0;
 
 function sendMsg()
 {
-    //send to server
-    var msg = messages[index];
-    index = index + 1;
-    if (index == messages.length)
+    var temperture = (Math.random() * 40).toFixed(2);
+    var humidity = (Math.random() * 100).toFixed(2);
+    var x = Math.ceil((Math.random() * 1000));
+    var y = Math.ceil((Math.random() * 1000));
+    var z = Math.ceil((Math.random() * 1000));
+    var msg;
+
+    if (index == 0)
     {
-        index = 0;
+        msg = `A01wd${temperture}sd${humidity}%B`;
+        index++;
     }
+    else {
+        msg = `A42X${x}Y${y}Z${z}B`
+        index = 0
+    }
+
     clientSocket.send(msg, 0, msg.length, 5683);
 }
 
