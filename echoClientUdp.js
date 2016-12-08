2493e9d453b2c3b7af68fe697f2874c66b70439e
WingedPanda <277698184@qq.com>
2016-12-08 14:08:37 +0800

    add sensor

--- echoClientUdp.js
+++ echoClientUdp.js
@@ -1,6 +1,6 @@
 var dgram = require('dgram');
 var clientSocket = dgram.createSocket('udp4');
-var sensortype = 01;
+var sensortype = 101;
 
 function sendMsg()
 {	
@@ -17,7 +17,7 @@ function sendMsg()
     var corrosion = 0.01;
 	var msg;
 	
-    if(sensortype === 01)
+    if(sensortype === 101 || sensortype === 102 || sensortype === 103 || sensortype === 104)
     {
         displacement += Math.random() - 0.5;
         if (displacement > 50)
@@ -29,25 +29,33 @@ function sendMsg()
             displacement = 0;
         }
         displacement = Math.round(displacement * 100) / 100;
-		msg = `A0101WY${displacement}B`;
-		sensortype = 02;		
+		msg = `A0${sensortype}WY${displacement}B`;
+		sensortype ++;
+		if (sensortype === 105) 
+		{
+			sensortype = 201;
+		}
     }
-    else if(sensortype === 02)
+    else if(sensortype === 201 || sensortype === 202 || sensortype === 203)
     {
-        deflection += Math.random()- 0.5;
-        if (deflection > 50)
+        displacement += Math.random() - 0.5;
+        if (displacement > 50)
         {
-            deflection = 50;
+            displacement = 50;
         }
-        else if (deflection < -10)
+        else if (displacement < 0)
         {
-            deflection = -10;
+            displacement = 0;
         }
-        deflection = Math.round(deflection * 100) / 100;
-		msg = `A0201ND${deflection}B`;
-		sensortype = 03;		
-    }
-    else if(sensortype === 03)
+        displacement = Math.round(displacement * 100) / 100;
+		msg = `A0${sensortype}ND${deflection}B`;
+		sensortype ++;
+		if (sensortype === 204) 
+		{
+			sensortype = 301;
+		}		
+    }	
+    else if(sensortype === 301 || sensortype === 302)
     {
         verticality += Math.random()*0.1 - 0.05;
         if (verticality > 90)
@@ -59,10 +67,20 @@ function sendMsg()
             verticality = 87;
         }
         verticality = Math.round(verticality * 100) / 100;
-		msg = `A0301CZD${verticality}B`;
-		sensortype = 04;		
+		msg = `A0${sensortype}CZD${verticality}B`;
+		sensortype ++;
+		if (sensortype === 303) 
+		{
+			sensortype = 401;
+		}		
     }
-    else if(sensortype === 04)
+    else if(sensortype === 401 || sensortype === 402 || sensortype === 403 || sensortype === 404 ||sensortype === 405 
+		|| sensortype === 406 || sensortype === 407 || sensortype === 408 || sensortype === 409 || sensortype === 410  
+		|| sensortype === 411 || sensortype === 412 || sensortype === 413 || sensortype === 414 || sensortype === 415 
+		|| sensortype === 416 || sensortype === 417 || sensortype === 418 || sensortype === 419 || sensortype === 420 
+		|| sensortype === 421 || sensortype === 422 || sensortype === 423 || sensortype === 424 || sensortype === 425 
+		|| sensortype === 426 || sensortype === 427 || sensortype === 428 || sensortype === 429 || sensortype === 430
+		|| sensortype === 431 || sensortype === 432)
     {
         strain += Math.random()*0.1 - 0.05;
         if (strain > 2)
@@ -74,10 +92,14 @@ function sendMsg()
             strain = 0;
         }
         strain = Math.round(strain * 100) / 100;
-		msg = `A0401YB${strain}B`;
-		sensortype = 05;		
+		msg = `A0${sensortype}YB${strain}B`;
+		sensortype ++;
+		if (sensortype === 433) 
+		{
+			sensortype = 501;
+		}		
     }
-    else if(sensortype === 05)
+    else if(sensortype === 501 || sensortype === 502 || sensortype === 503 || sensortype === 504 || sensortype === 505 || sensortype === 506)
     {
         cableforce += Math.random()*100 - 50;
         if (cableforce > 700)
@@ -89,10 +111,14 @@ function sendMsg()
             cableforce = 500;
         }
         cableforce = Math.round(cableforce * 10) / 10;
-		msg = `A0501SL${cableforce}B`;
-		sensortype = 06;		
+		msg = `A0${sensortype}SL${cableforce}B`;
+		sensortype ++;
+		if (sensortype === 507) 
+		{
+			sensortype = 601;
+		}		
     }
-    else if(sensortype === 06)
+    else if(sensortype === 601 || sensortype === 602 || sensortype === 603 || sensortype === 604 || sensortype === 605 || sensortype === 606)
     {
         x += Math.random()*100 - 50;
         y += Math.random()*100 - 50;
@@ -124,10 +150,14 @@ function sendMsg()
             z = 0;
         }
         z = Math.round(z * 10) / 10;
-		msg = `A0601X${x}Y${y}Z${z}B`;
-		sensortype = 07;		
+		msg = `A0${sensortype}X${x}Y${y}Z${z}B`;
+		sensortype ++;
+		if (sensortype === 607) 
+		{
+			sensortype = 701;
+		}		
     }
-    else if(sensortype === 07)
+    else if(sensortype === 701 || sensortype === 702)
     {
         temperature += Math.random() - 0.5;
         if (temperature > 15)
@@ -150,23 +180,29 @@ function sendMsg()
             humidity = 10;
         }
         humidity = Math.round(humidity * 10) / 10;
-		msg = `A0701wd${temperature}sd${humidity}%B`;
-		sensortype = 08;
+		msg = `A0${sensortype}wd${temperature}sd${humidity}%B`;
+		sensortype ++;
+		if (sensortype === 703) 
+		{
+			sensortype = 801;
+		}
     }
-    else if(sensortype === 08)
+    else if(sensortype === 801 || sensortype === 802)
     {
         corrosion = 0.01;
-		msg = `A0801FS${corrosion}B`;
-		sensortype = 01;
+		msg = `A0${sensortype}FS${corrosion}B`;
+		sensortype ++;
+		if (sensortype === 803) 
+		{
+			sensortype = 101;
+		}
     }
-
-	console.log(msg);	
 
     clientSocket.send(msg, 0, msg.length, 5683);
 }
 
 //start a timer to send message to echoServer
-setInterval(sendMsg, 2000);
+setInterval(sendMsg, 85);
 
 clientSocket.on('message', function (msg, rinfo)
 {
