0dfb2bb5c42da897a2452ee1877215296be5159e
WingedPanda <277698184@qq.com>
2017-01-19 09:28:29 +0800

    update

--- echoClientUdp.js
+++ echoClientUdp.js
@@ -6,15 +6,15 @@ var sensortype3 = 701;
 var sensortype4 = 601;
 var sensortype5 = 801;
 var sensortype6 = 901;
-var x = 100;
-var y = 100;
-var z = 100;
+var x = 35;
+var y = 40;
+var z = -998;
 var sinvalue = 0;
-var displacement = 7;
-var verticality = 90;
-var cableforce = 650;
+var displacement = -7;
+var verticality = 0;
+var cableforce = 950;
 var temperature = 10;
-var humidity = 30;
+var humidity = 35;
 var corrosion = 0.01;
 var lane = '01';
 var index = 1;
@@ -25,14 +25,14 @@ function sendMsgDisplacementAndVerticality()
 	
     if(sensortype1 === 101 || sensortype1 === 102 || sensortype1 === 103 || sensortype1 === 104)
     {
-        displacement += Math.random() - 0.5;
-        if (displacement > 50)
+        displacement += Math.random()*20 - 10;
+        if (displacement > 0)
         {
-            displacement = 50;
+            displacement = -7;
         }
-        else if (displacement < 0)
+        else if (displacement < -30)
         {
-            displacement = 0;
+            displacement = -10;
         }
         displacement = Math.round(displacement * 100) / 100;
 		msg = `A0${sensortype1}WY${displacement}B\r\n`;
@@ -45,13 +45,13 @@ function sendMsgDisplacementAndVerticality()
 	else if(sensortype1 === 301 || sensortype1 === 302)
     {
         verticality += Math.random()*0.1 - 0.05;
-        if (verticality > 90)
+        if (verticality > 2)
         {
-            verticality = 90;
+            verticality = 0.5;
         }
-        else if (verticality < 87)
+        else if (verticality < -2)
         {
-            verticality = 87;
+            verticality = -0.3;
         }
         verticality = Math.round(verticality * 100) / 100;
 		msg = `A0${sensortype1}CZD${verticality}B\r\n`;
@@ -62,7 +62,7 @@ function sendMsgDisplacementAndVerticality()
 		}		
     }	
 
-    clientSocket.send(msg, 0, msg.length, 5683);
+    clientSocket.send(msg, 0, msg.length, 5682);
 	console.log(msg,msg.length)
 }
 
@@ -72,14 +72,14 @@ function sendMsgCableforce()
 	
 	if(sensortype2 === 501 || sensortype2 === 502 || sensortype2 === 503 || sensortype2 === 504 || sensortype2 === 505 || sensortype2 === 506)
     {
-        cableforce += Math.random()*100 - 50;
-        if (cableforce > 700)
+        cableforce += Math.random()*600 - 300;
+        if (cableforce > 1500)
         {
-            cableforce = 700;
+            cableforce = 870;
         }
-        else if (cableforce < 500)
+        else if (cableforce < 800)
         {
-            cableforce = 500;
+            cableforce = 935;
         }
         cableforce = Math.round(cableforce * 10) / 10;
 		msg = `A0${sensortype2}SL${cableforce}B\r\n`;
@@ -90,7 +90,7 @@ function sendMsgCableforce()
 		}		
     }
 
-    clientSocket.send(msg, 0, msg.length, 5683);
+    clientSocket.send(msg, 0, msg.length, 5682);
 	console.log(msg,msg.length)
 }
 
@@ -112,13 +112,13 @@ function sendMsgTemperature()
         temperature = Math.round(temperature * 10) / 10;
 
         humidity += Math.random() - 0.5;
-        if (humidity > 50)
+        if (humidity > 90)
         {
-            humidity = 50;
+            humidity = 75;
         }
         else if (humidity < 10)
         {
-            humidity = 10;
+            humidity = 28;
         }
         humidity = Math.round(humidity * 10) / 10;
 		msg = `A0${sensortype3}WD${temperature}SD${humidity}B\r\n`;
@@ -129,7 +129,7 @@ function sendMsgTemperature()
 		}
     }
 
-    clientSocket.send(msg, 0, msg.length, 5683);
+    clientSocket.send(msg, 0, msg.length, 5682);
 	console.log(msg,msg.length)
 }
 
@@ -139,34 +139,37 @@ function sendMsgAcc()
 	
     if(sensortype4 === 601 || sensortype4 === 602 || sensortype4 === 603 || sensortype4 === 604 || sensortype4 === 605 || sensortype4 === 606)
     {
-        x = Math.sin(sinvalue* 0.017453293 )*100 + Math.random()*100 - 50;
+/*         x = Math.sin(sinvalue* 0.017453293 )*100 + Math.random()*100 - 50;
         y = Math.sin(sinvalue* 0.017453293 )*100 + Math.random()*100 - 50;
-        z = Math.sin(sinvalue* 0.017453293 )*100 + Math.random()*100 - 50;	
-        if (x > 700)
+        z = Math.sin(sinvalue* 0.017453293 )*100 + Math.random()*100 - 50; */
+        x += Math.random()*100 - 20;
+        y += Math.random()*100 - 20;
+        z += Math.random()*100 - 20;			
+        if (x > 300)
         {
-            x = 700;
+            x = 289;
         }
-        else if (x < -700)
+        else if (x < 0)
         {
-            x = -700;
+            x = 15;
         }
         x = Math.round(x * 10) / 10;
-        if (y > 700)
+        if (y > 300)
         {
-            y = 700;
+            y = 289;
         }
-        else if (y < -700)
+        else if (y < 0)
         {
-            y = -700;
+            y = 15;
         }
         y = Math.round(y * 10) / 10;
-        if (z > 700)
+        if (z > -700)
         {
-            z = 700;
+            z = -721;
         }
-        else if (z < -700)
+        else if (z < -1300)
         {
-            z = -700;
+            z = -1287;
         }
         z = Math.round(z * 10) / 10;
 		sinvalue += 30;
@@ -182,7 +185,7 @@ function sendMsgAcc()
 			sensortype4 = 601;
 		}	
 		msg = `A0${sensortype4}X${x}Y${y}Z${z}B\r\n`;
-		clientSocket.send(msg, 0, msg.length, 5683);
+		clientSocket.send(msg, 0, msg.length, 5682);
 		console.log(msg,msg.length)		
     }
 
@@ -202,7 +205,7 @@ function sendMsgCorrosion()
 		}
     }
 
-    clientSocket.send(msg, 0, msg.length, 5683);
+    clientSocket.send(msg, 0, msg.length, 5682);
 	console.log(msg,msg.length)
 }
 
@@ -238,8 +241,7 @@ function sendMsgtrafficload()
 	{
 		licenseplate = 'CBD54333363334350000';
 		index++;
-	}
-	else if(index === 6)
+	}else if(index === 6)
 	{
 		licenseplate = 'CBD54B35363333350000';
 		index++;
@@ -258,13 +260,37 @@ function sendMsgtrafficload()
 	
 	var weight;
 	var time = '07e00101091414';
-	var weightlimit = '0770';
+	var weightlimit = 'afc8';
 	var overweight;
 	var overweighttag;
 	var axesweight = ['0000','0000','0000','0000','0000','0000','0000','0000'];
 	for (let i = 0; i < axesnumber; i++)
 	{
-		axesweight[i] = (Math.round(Math.random()*1000)).toString(16);
+		if (i == 0)
+		{
+			axesweightvalue = Math.round(Math.random()*1000);
+			if(axesweightvalue < 500)
+			{
+				axesweightvalue = axesweightvalue + 300;
+			}
+			axesweight[i] = axesweightvalue.toString(16);
+		}else if (i == axesnumber-1)
+		{
+			axesweightvalue = Math.round(Math.random()*1000);
+			if(axesweightvalue < 500)
+			{
+				axesweightvalue = axesweightvalue + 300;
+			}
+			axesweight[i] = axesweightvalue.toString(16);
+		}else
+		{
+			axesweightvalue = Math.round(Math.random()*5000);
+			if(axesweightvalue < 1000)
+			{
+				axesweightvalue = axesweightvalue + 1000;
+			}
+			axesweight[i] = axesweightvalue.toString(16);
+		}
 		axesweight[i] = padLeft(axesweight[i],4);
 	}
 	weight = (parseInt(axesweight[0], 16) + parseInt(axesweight[1], 16) + parseInt(axesweight[2], 16) + parseInt(axesweight[3], 16)
@@ -282,7 +308,15 @@ function sendMsgtrafficload()
 	var axesvelocity = ['00','00','00','00','00','00','00','00'];
 	for (let i = 0; i < axesnumber; i++)
 	{
-		axesvelocity[i] = (Math.round(Math.random()*200)).toString(16);
+		if (i == 0)
+		{
+			axesvelocityvalue = Math.round(Math.random()*120);
+			axesvelocity[i] = axesvelocityvalue.toString(16);
+			
+		}else{
+			axesvelocityvalue = axesvelocityvalue + Math.round(Math.random()*12) - 6;
+			axesvelocity[i] = axesvelocityvalue.toString(16);
+		}
 		axesvelocity[i] = padLeft(axesvelocity[i],2);
 	}
 	var shaftspacing = ['0000','0000','0000','0000','0000','0000','0000','0000'];
@@ -331,8 +365,8 @@ function sendMsgtrafficload()
 setInterval(sendMsgDisplacementAndVerticality, 1000*10);
 setInterval(sendMsgCableforce, 1000*10);
 setInterval(sendMsgTemperature, 1000*30);
-setInterval(sendMsgAcc, 1500);
-setInterval(sendMsgCorrosion, 1000*60*20); 
+setInterval(sendMsgAcc, 700);
+setInterval(sendMsgCorrosion, 1000); //1000*60*20
 setInterval(sendMsgtrafficload, 1000*5);
 
 clientSocket.on('message', function (msg, rinfo)
